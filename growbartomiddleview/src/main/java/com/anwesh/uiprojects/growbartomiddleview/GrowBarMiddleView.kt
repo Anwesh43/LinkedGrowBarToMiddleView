55726a0a5625fa31d01e3018149432764f4c4450
Anwesh43 <anweshthecool0@gmail.com>
2020-06-05 08:20:01 +0530

    adding GBMNode class

--- growbartomiddleview/src/main/java/com/anwesh/uiprojects/growbartomiddleview/GrowBarMiddleView.kt
+++ growbartomiddleview/src/main/java/com/anwesh/uiprojects/growbartomiddleview/GrowBarMiddleView.kt
@@ -117,4 +117,45 @@ class GrowBarMiddleView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class GBMNode(var i : Int, val state : State = State()) {
+
+        private var next : GBMNode? = null
+        private var prev : GBMNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = GBMNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawGMBNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : GBMNode {
+            var curr : GBMNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
