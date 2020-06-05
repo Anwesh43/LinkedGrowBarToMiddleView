1044eecf1664e643f17761f30e702b092a8f5963
Anwesh43 <anweshthecool0@gmail.com>
2020-06-05 07:40:18 +0530

    adding constants and libraries

--- growbartomiddleview/src/main/java/com/anwesh/uiprojects/growbartomiddleview/GrowBarMiddleView.kt
+++ growbartomiddleview/src/main/java/com/anwesh/uiprojects/growbartomiddleview/GrowBarMiddleView.kt
@@ -3,3 +3,19 @@ package com.anwesh.uiprojects.growbartomiddleview
 /**
  * Created by anweshmishra on 05/06/20.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.RectF
+
+val colors : Array<String> = arrayOf("#3F51B5", "#4CAF50", "#2196F3", "#009688", "#F44336")
+val bars : Int = 5
+val scGap : Float = 0.02f / bars
+val backColor : Int = Color.parseColor("#BDBDBD")
+val delay : Long = 20
+val rot : Float = 90f
