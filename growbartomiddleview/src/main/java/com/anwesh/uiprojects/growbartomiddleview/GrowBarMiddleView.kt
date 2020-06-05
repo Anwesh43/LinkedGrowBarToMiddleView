dfdeffa4bd9974a917d2703c1861fc8e5246ecd4
Anwesh43 <anweshthecool0@gmail.com>
2020-06-05 08:21:54 +0530

    adding GrowBarToMiddle class

--- growbartomiddleview/src/main/java/com/anwesh/uiprojects/growbartomiddleview/GrowBarMiddleView.kt
+++ growbartomiddleview/src/main/java/com/anwesh/uiprojects/growbartomiddleview/GrowBarMiddleView.kt
@@ -158,4 +158,27 @@ class GrowBarMiddleView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class GrowBarToMiddle(var i : Int) {
+
+        private var curr : GBMNode = GBMNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
